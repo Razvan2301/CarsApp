afa598cb6efb2feb060506198f6af7e6504da658
razvan2301 <razvan2301@gmail.com>
2018-02-14 19:46:59 +0200

    create search

--- src/main/java/com/cardealers/cardealers/repository/CarRepository.java
+++ src/main/java/com/cardealers/cardealers/repository/CarRepository.java
@@ -4,6 +4,9 @@ import com.cardealers.cardealers.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findByBrand(String brandName);
 }
